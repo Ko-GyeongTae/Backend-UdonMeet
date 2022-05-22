8a87be73fe071bffa785c4d219b7d073956ec1b1
Ko-GyeongTae <kokt0203@naver.com>
2022-05-22 18:45:49 +0900

    Feat: Service 계층 구조 수정 - Repository 의존성 제거 - Arrow Function 형태로 변경 - 회원 탈퇴 Business Logic 추가

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,28 +1,60 @@
 import Container, { Service } from 'typedi';
-import { customUserRepository } from '../loaders/container';
-import { SignInDto, SignUpDto } from '../models/dto/user.dto';
+import crypto from 'crypto';
+import { JwtDto, SignInDto, SignUpDto } from '../models/dto/user.dto';
+import { User } from '../models/entity/user.entity';
 import { CustomUserRepository } from '../models/repository/user.repository';
+import { createJWT } from '../utils/jwt';
 
 @Service()
 export class UserService {
-  private userRepository = customUserRepository;
+  //private userRepository = customUserRepository;
+  constructor(private userRepository: CustomUserRepository) {}
 
-  async signIn(body: SignInDto) {
+  signIn = async (body: SignInDto): Promise<JwtDto | null> => {
     const { email, password } = body;
-    return await this.userRepository.findOneExistUserByEmail(email);
-  }
-  async signUp(body: SignUpDto) {
-    if (!(await this.userRepository.findOneExistUserByEmail(body.email))) {
-      return new Error('Email is already Exist');
+    const user = await this.userRepository.findOneExistUserByEmail(email);
+    if (!user) {
+      return null;
     }
-    const res = await this.userRepository.insertUserWithHashing(body);
-    console.log(res);
-    return res;
-  }
-  async signOut() {
-    return 'hello';
-  }
-  async withDrawal() {
-    return 'hello';
-  }
+
+    const hash = crypto.createHash('sha512').update(password).digest('hex');
+
+    if (user.password !== hash) {
+      return null;
+    }
+
+    const payload = {
+      id: user.id,
+      email: user.email,
+      name: user.name,
+    };
+
+    const jwtDto = new JwtDto();
+    jwtDto.accessToken = createJWT(payload, '5h');
+    jwtDto.refreshToken = createJWT({}, '14d', 'HS512');
+    return jwtDto;
+  };
+  signUp = async (body: SignUpDto): Promise<User | null> => {
+    const result = await this.userRepository.insertUserWithHashing(body);
+    console.log(result);
+    if (result) {
+      return result;
+    } else {
+      return null;
+    }
+  };
+  signOut = async () => {
+    // const sessionRepository = await getRepository(Session);
+    // try {
+    //   await sessionRepository.delete(req.cookies.refreshToken);
+    // } catch (e) {
+    //   res.status(400);
+    //   return;
+    // }
+  };
+  withDrawal = async (): Promise<boolean> => {
+    return await this.userRepository.softDeleteUserById(
+      'f6a2f240-1dce-41c0-b216-fa49b6395079',
+    );
+  };
 }
