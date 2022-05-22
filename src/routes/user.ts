d9d64aa83f50c248369a11c1e38ed1b1523524bf
Ko-GyeongTae <kokt0203@naver.com>
2022-05-22 18:42:24 +0900

    Feat: 라우트 수정 - 로그아웃 및 탈퇴 라우팅 추가 - 미들웨어 적용

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import Container from 'typedi';
 import { UserController } from '../controllers/user.controller';
 import { SignInDto, SignUpDto } from '../models/dto/user.dto';
+import { jwtMiddleware } from '../utils/jwtMiddleware';
 import { validateBodyMiddleware } from '../utils/validateMiddleware';
 
 const router = Router();
@@ -18,5 +19,7 @@ router.post(
   validateBodyMiddleware(SignUpDto, '회원가입 형식이 잘못되었습니다.'),
   userController.signUp,
 );
+router.put('/sign-out', jwtMiddleware, userController.signOut);
+router.delete('/with-drawl', jwtMiddleware, userController.withDrawl);
 
 export default router;
