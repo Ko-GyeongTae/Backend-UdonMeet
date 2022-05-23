9db65274c445aadc91578c2d9bced7b3fc29ef38
Ko-GyeongTae <kokt0203@naver.com>
2022-05-23 11:01:25 +0900

    Chore: Express Request User 타입 추가

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,9 +1,15 @@
 export {};
 
+export interface globalUser {
+  id: string;
+  email: string;
+  name: string;
+}
+
 declare global {
   namespace Express {
     export interface Request {
-      user: any;
+      user: globalUser;
     }
   }
 
